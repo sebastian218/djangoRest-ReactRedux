267a7bd8a74c463d62c0588b231b7894ddc6b790
Sebastian Rodero <sebastianrodero@MacBook-Pro-de-Sebastian.local>
2019-09-28 16:18:03 -0300

    first commit

---
+++ leadmanager/leads/api.py
@@ -0,0 +1,13 @@
+from .models import Lead
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+
+# Lead Viewset
+
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    serializer_class = LeadSerializer
+    permissions_classes = [
+        permissions.AllowAny
+    ]
